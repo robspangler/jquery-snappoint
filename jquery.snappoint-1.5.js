366947cf7fb817bbacde6683d8b14c94e825daf4
ken frederick <ken.frederick@gmx.de>
2013-04-11 12:44:35 +0200

    added jQuery.Browser workaround for jQuery 1.9.0+

--- jquery.snappoint-1.5.js
+++ jquery.snappoint-1.5.js
@@ -18,6 +18,17 @@
  */
 (function( $ ){
 
+	//
+	// as of jQuery 1.9.0
+	// jQuery.browser is out
+	// http://pupunzi.open-lab.com/2013/01/16/jquery-1-9-is-out-and-browser-has-been-removed-a-fast-workaround/
+	//
+	jQuery.browser = {};
+	jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase());
+	jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
+	jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
+	jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
+
 	var snapPointTimer = null;
 	
 	$.fn.snapPoint = function(options) {
